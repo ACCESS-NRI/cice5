679550913bc6c243f24a12332bc5a7ea6c892ee2
Spencer Wong <spencer.wong@anu.edu.au>
2025-07-29 16:54:03 +1000

    Use iomsg in error message

--- drivers/access/cpl_parameters.F90
+++ drivers/access/cpl_parameters.F90
@@ -207,15 +207,16 @@ character (len=256) :: errstr, tmpstr         ! For holding namelist read errors
 ! all processors read the namelist--
 
 call get_fileunit(nu_nml)
-open(unit=nu_nml,file="input_ice.nml",form="formatted",status="old",iostat=nml_error)
+open(unit=nu_nml,file="input_ice.nml",form="formatted",status="old",iostat=nml_error, iomsg=errstr)
 !
 if (my_task == master_task) then
    write(ice_stdout,*)'CICE: input_ice.nml opened at unit = ', nu_nml
 endif
 !
 if (nml_error /= 0) then
-   write(errstr, '(a,i3)') 'CICE: ERROR failed to open input_ice.nml. Error code: ', nml_error
-   call abort_ice(trim(errstr))
+   write(tmpstr, '(a,i3,a)') 'CICE: ERROR failed to open input_ice.nml. Error code: ', nml_error, &
+                            '  - ' // trim(errstr)
+   call abort_ice(trim(tmpstr))
 else
    nml_error =  1
 endif
