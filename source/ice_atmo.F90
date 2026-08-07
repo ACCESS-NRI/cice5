3cca03ab714142fdaf2885b933a2773d08883107
anton-seaice <anton.steketee@anu.edu.au>
2026-08-07 11:01:11 +1000

    typo

--- source/ice_atmo.F90
+++ source/ice_atmo.F90
@@ -57,7 +57,7 @@
          Cdn_atm_ratio   ! ratio drag atm / neutral drag atm
 !ars599: 24092014 (CODE: petteri)
       ! tuning parameters, set in namelist
-#if defined(AUSCOM)
+#if defined(AusCOM)
       real (kind=dbl_kind), public :: &
          iceruf    ! ice surface roughness (m)
 #endif
